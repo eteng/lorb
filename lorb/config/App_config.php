48e5ca051e8f62d2bf25dd5bd136aa008938049b
Eteng omini <Eteng_40@yahoo.com>
2009-09-23 10:40:51 +0000

    Add ons to Framework

---
+++ lorb/config/App_config.php
@@ -0,0 +1,9 @@
+<?php
+//the Application base URL 
+define('AppURL',"lorb/lorb");
+//the Application Name
+define('AppName',"Lorb");
+
+
+
+?>
---
+++ lorb/config/App_const.php
@@ -0,0 +1,15 @@
+<?php
+/**this file will hold Application constants
+ */
+ 
+//Directory Seperator
+define('DS','\\'); 
+//Path Seperator
+define('PS','/'); 
+//config Path
+define('CFGROOT',dirname(__FILE__));
+//root path
+define('APPROOT',dirname(CFGROOT.'..'));
+
+//echo APPROOT."<br />".CFGROOT;
+?>
