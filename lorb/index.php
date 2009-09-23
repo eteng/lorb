89a4b831e604adb28f157951f2e8e5a89d928227
Eteng omini <Eteng_40@yahoo.com>
2009-09-23 10:08:49 +0000

    Checking routing

--- lorb/index.php
+++ lorb/index.php
@@ -6,7 +6,43 @@
     </head>
     <body>
         <?php
-        // put your code here
+		require_once 'config/App_config.php';
+		require_once 'config/App_const.php';
+		
+		define('b',"<br />");
+        
+        echo "welome to main page".b;
+		
+		//get the request url	
+		$url = $_SERVER['REQUEST_URI'];
+		//remove application url from the request url 
+		$url = str_replace(PS.AppURL.PS,'',$url);
+        //Matching the url
+        $routes = array(
+                        array('url'=>'/^post\/(?P<id>\d+)$/','controller' =>'post','view'=>'show'),
+                        array('url'=>'/^post\/(?P<id>\d+\/edit)$/','controller' =>'post','view'=>'edit')
+                        );
+       // $pattern ='/^post\/(?P<id>\d+\/edit)$/';
+
+       // $pattern ='/^(?P<comp>\w+)\/(?P<id>\d+)\/(?P<action>\w+)$/'; //for further development
+       $params = array();
+       foreach($routes as $urls =>$route)
+       {
+          if(preg_match($route['url'],$url,$matches))
+          {
+              $params = array_merge($params,$matches);
+              break;
+          }
+       }
+
+        echo b."request = ".$url.b.b;
+        dox($params);
+
+        function dox($params){
+            foreach($params as $key => $value){
+				echo $key." ==> ".$value."<br />";
+			}
+        }
         ?>
     </body>
 </html>
