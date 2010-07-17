48a9360d69ed876a05f3f02c5867dd78141bc369
Eteng omini <Eteng_40@yahoo.com>
2010-07-17 19:23:58 +0000

    rest

--- lorb/index.php
+++ lorb/index.php
@@ -2,7 +2,6 @@
 $_time = microtime(true);
 /**turn error reporting on**/
 error_reporting(E_ALL);
-
 //site path constant
 $site_path = realpath(dirname(__FILE__));
 define('__SITE_PATH',$site_path);
@@ -24,12 +23,12 @@ include 'sys/boot.php';
 
 /*** load the controller ***/
 $registry->front->start();
-if($registry->front->getController()!= 'mxfactory'){
-print "<br />\n| Memory peak usage: ".memory_get_peak_usage();
-print " | Memory peak usage(true): ".memory_get_peak_usage(true);
-print " | Memory usage: ".memory_get_usage();
-print " | Memory usage(true): ".memory_get_usage(true);
-printf ("\n\r | Time elapsed: %s seconds",microtime(true) - $_time);
-}
+//if($registry->front->getController()!= 'mxfactory'){
+//print "<br />\n| Memory peak usage: ".memory_get_peak_usage();
+//print " | Memory peak usage(true): ".memory_get_peak_usage(true);
+//print " | Memory usage: ".memory_get_usage();
+//print " | Memory usage(true): ".memory_get_usage(true);
+//printf ("\n\r | Time elapsed: %s seconds",microtime(true) - $_time);
+//}
 ?>
 
