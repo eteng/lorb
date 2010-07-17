d91f4c205b4137170a7c5af725c77709a8c8d245
Eteng omini <Eteng_40@yahoo.com>
2010-07-17 19:01:08 +0000

    city sevice

---
+++ lorb/lib/Dodeye/core/template/AbstractTpl.php
@@ -0,0 +1,13 @@
+<?php
+pitch("Dodeye.core.dataAccess.Alice");
+/**
+ * Description of AbstractTpl
+ * @author Eteng Omini <e-t-e-n-g@hotmail.com> 
+ */
+class AbstractTpl extends Alice{
+    private $name;
+    function getDefaultTpl(){
+
+    }
+}
+?>
